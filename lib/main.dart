bfa1b607b1b9143e872148c2a6e1e8d78e47d1d6
benisburgers <benibargera@gmail.com>
2020-04-17 16:30:34 +0200

    Temp commit: Works but buggy af

--- lib/main.dart
+++ lib/main.dart
@@ -1,132 +1,176 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:phone_state_i/phone_state_i.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/cupertino.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Startup Name Generator',
-      theme: ThemeData(
-        primaryColor: Colors.white,
+    return new MaterialApp(
+      title: 'Sensors Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      home: RandomWords(),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>(); // Add this line.
-  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+class MyHomePage extends StatefulWidget {
+  final String title;
 
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
+  MyHomePage({Key key, this.title}) : super(key: key);
 
-          final List<Widget> divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
 
-          return Scaffold(
-            // Add 6 lines from here...
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          ); // ... to here.
-        },
-      ),
-    );
-  }
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+  StreamSubscription streamSubscription;
+  AppLifecycleState _notification;
+  bool _activeCall = false;
 
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16),
-        // The itemBuilder callback is called once per suggested
-        // word pairing, and places each suggestion into a ListTile
-        // row. For even rows, the function adds a ListTile row for
-        // the word pairing. For odd rows, the function adds a
-        // Divider widget to visually separate the entries. Note that
-        // the divider may be difficult to see on smaller devices.
-        itemBuilder: (BuildContext _context, int i) {
-          // Add a one-pixel-high divider widget before each row
-          // in the ListView.
-          if (i.isOdd) {
-            return Divider();
-          }
-
-          // The syntax "i ~/ 2" divides i by 2 and returns an
-          // integer result.
-          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
-          // This calculates the actual number of word pairings
-          // in the ListView,minus the divider widgets.
-          final int index = i ~/ 2;
-          // If you've reached the end of the available word
-          // pairings...
-          print(_suggestions);
-          if (index >= _suggestions.length) {
-            // ...then generate 10 more and add them to the
-            // suggestions list.
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        // Add 9 lines from here...
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
+  var flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: <Widget>[
-          // Add 3 lines from here...
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ], // ... to here.
+    print('setstate');
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text('Example'),
       ),
-      body: _buildSuggestions(),
+      body: new Padding(
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            new Text('A:'),
+            new FlatButton(
+                onPressed: () {
+                  setState(() {
+                    print('refresh');
+                  });
+                },
+                child: Text('Refresh'))
+          ],
+        ),
+        padding: const EdgeInsets.all(16.0),
+      ),
     );
   }
-}
 
-class RandomWords extends StatefulWidget {
   @override
-  RandomWordsState createState() => RandomWordsState();
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print('didChangeAppLifecycleState');
+    print(state);
+    setState(() {
+      _notification = state;
+    });
+    switch (state) {
+      case AppLifecycleState.resumed:
+        resumeCallback();
+        break;
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.paused:
+      case AppLifecycleState.detached:
+        break;
+    }
+  }
+
+  @override
+  void dispose() {
+    streamSubscription.cancel();
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+
+    phoneStateCallEvent.listen((PhoneStateCallEvent event) {
+      // print('Call is Incoming or Connected' + event.stateC);
+      if (event.stateC == 'true') {
+        // print('yolo ' + event.stateC);
+        _activeCall = true;
+        _showNotification();
+      } else {
+        _activeCall = false;
+      }
+      //event.stateC has values "true" or "false"
+    });
+
+    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+    var initializationSettingsIOS = IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    var initializationSettings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+    );
+
+    streamSubscription =
+        phoneStateCallEvent.listen((PhoneStateCallEvent event) {
+      // print('Call is Incoming or Connected' + event.stateC);
+      if (event.stateC == 'true') {
+        // print('yolo ' + event.stateC);
+        _showNotification();
+      }
+      //event.stateC has values "true" or "false"
+    });
+  }
+
+  Future onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) async {
+    // display a dialog with the notification details, tap ok to go to another page
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => CupertinoAlertDialog(
+        title: Text(title),
+        content: Text(body),
+        actions: [
+          CupertinoDialogAction(
+            isDefaultAction: true,
+            child: Text('Ok'),
+            onPressed: () async {
+              print('hello');
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  Future _showNotification() async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        importance: Importance.Max, priority: Priority.High);
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+
+//    await flutterLocalNotificationsPlugin.show(
+//        0, 'plain title', 'plain body', platformChannelSpecifics,
+//        payload: 'item x');
+
+    String trendingNewsId = '5';
+    await flutterLocalNotificationsPlugin.show(0, 'Call happening - yolo 123',
+        'Click here to record.', platformChannelSpecifics,
+        payload: trendingNewsId);
+  }
+
+  Future resumeCallback() async {
+    print('resumeCallback');
+    if (_activeCall) {
+      launch("tel://21213123123");
+    }
+  }
 }
